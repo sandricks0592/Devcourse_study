50e345292162716f189800394cecd665b4cea628
jihwan <jihwan0713@naver.com>
2026-01-13 20:27:53 +0900

    project final(1/13) - delete database

--- web_start/index.js
+++
@@ -1,7 +0,0 @@
-let server = require('./server');
-let router = require('./router');
-let request_Handler = require('./request_Handler');
-const mariadb = require('./database/connect/mariadb.js');
-mariadb.connect();
-
-server.start(router.route, request_Handler.handle);
